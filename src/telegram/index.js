0e47bb870f60fb980e336d33190c302af2aa307d
Stanislav Kutyepov <s.kutyepov@gmail.com>
2019-12-03 23:00:46 -0500

    fix(console-warning): suppress warning about promises from node-telegram-bot-api

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,3 +1,9 @@
+// temporary fix to suppress warning from node-telegram-bot-api
+// about auto enabling promise cancellation. This can be removed
+// once node-telegram-bot-api@1.0.0 is released.
+// details: https://github.com/yagop/node-telegram-bot-api/issues/319
+process.env.NTBA_FIX_319 = true;
+
 const TelegramBot = require('node-telegram-bot-api');
 const logger = require('../utils/logger')(__filename);
 const Subscriptions = require('./subscriptions');
