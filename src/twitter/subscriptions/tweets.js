65c54eaf85decb1c769646c3d67ad7e136dc58d0
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-01-03 14:29:25 -0500

    fix(tweets): reconnect to twitter's stream if disconnected

--- src/twitter/subscriptions/tweets.js
+++ src/twitter/subscriptions/tweets.js
@@ -2,39 +2,82 @@ const Kefir = require('kefir');
 const logger = require('../../utils/logger')(__filename);
 
 const FOLLOWING = 25073877; //@realdonaldtrump
+const RECONNECT_TIMEOUT = 5000;
 
-function Stream(twitterClient) {
-    logger.info('Initialized subscription to stream of tweets');
+class StreamOfTweets {
+    constructor(emitter, twitterClient) {
+        this.emitter = emitter;
+        this.twitterClient = twitterClient;
 
-    const streamOfTweets = twitterClient.stream('statuses/filter', {follow: FOLLOWING.toString()});
+        this.streamOfTweets = null;
+        this.create = this.create.bind(this);
+        this.onError = this.onError.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+        this.onNewTweet = this.onNewTweet.bind(this);
+    }
 
-    return Kefir.stream((emitter) => {
-        function onNewTweet(event) {
-            if (event && event.text && event.user && event.user.id) {
-                if (event.user.id === FOLLOWING) {
-                    logger.info(`New tweet ${event.text} from "${event.user.name}" "${event.user.id}"`);
+    create() {
+        logger.info('Initialized subscription to stream of tweets');
 
-                    emitter.emit(event.text);
-                }
+        this.streamOfTweets = this.twitterClient.stream('statuses/filter', {follow: FOLLOWING.toString()});
+        this.streamOfTweets.on('data', this.onNewTweet);
+        this.streamOfTweets.on('error', this.onError);
+        this.streamOfTweets.on('end', this.onEnd);
+    }
+
+    onNewTweet(event) {
+        if (event && event.text && event.user && event.user.id) {
+            if (event.user.id === FOLLOWING) {
+                logger.info(`New tweet ${event.text} from "${event.user.name}" "${event.user.id}"`);
+
+                this.emitter.emit(event.text);
             }
         }
+    }
 
-        streamOfTweets.on('data', onNewTweet);
+    onError(error) {
+        logger.error('Twitter API error', error);
+        this.emitter.error(error);
+        this.reconnect();
+    }
 
-        streamOfTweets.on('error', (error) => {
-            logger.error('Twitter API error', error);
+    onEnd(response) {
+        logger.error('Twitter stream closed', response);
+        this.reconnect();
+    }
 
-            emitter.error(error);
-        });
+    reconnect() {
+        this.destroy();
+        clearTimeout(this.reconnectTimerId);
+        this.reconnectTimerId = setTimeout(this.create, RECONNECT_TIMEOUT);
+    }
 
-        streamOfTweets.on('end', (response) => {
-            logger.error('Twitter stream closed', response);
-        });
+    destroy() {
+        logger.info('Destroyed subscription to stream of tweets');
+
+        if (!this.streamOfTweets) {
+            return;
+        }
+
+        this.streamOfTweets.off('data', this.onNewTweet);
+        this.streamOfTweets.off('error', this.onError);
+        this.streamOfTweets.off('end', this.onEnd);
+
+        this.streamOfTweets.destroy();
+
+        this.streamOfTweets = null;
+    }
+}
+
+function Stream(twitterClient) {
+
+    return Kefir.stream((emitter) => {
+
+        const stream = new StreamOfTweets(emitter, twitterClient);
+        stream.create();
 
         return () => {
-            logger.info('Destroyed subscription to stream of tweets');
-
-            streamOfTweets.off('data', onNewTweet);
+            stream.destroy();
         }
     });
 }
